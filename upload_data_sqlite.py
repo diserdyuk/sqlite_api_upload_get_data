6a8bf8e12aaaaf0e7a17f2e2b8ea263b5be6c021
Denis Serdyuk <diserdyuk@gmail.com>
2021-03-10 02:20:22 +0200

    Got continent, country at ip from api geolocation

--- upload_data_sqlite.py
+++ upload_data_sqlite.py
@@ -1,6 +1,6 @@
-import gdown, csv
-from gsheets import *
+import csv
 import sqlite3
+import requests
 
 
 def upload_to_sqlite():
@@ -11,7 +11,6 @@ def upload_to_sqlite():
 
     with open('sessions.csv','r') as f:
         dr = csv.DictReader(f)
-        print(dr)
         to_db = [(i['id'], i['ip_address'], i['date']) for i in dr]
 
     cur.executemany("INSERT INTO data (id,ip_address,date) VALUES (?, ?, ?);", to_db)
@@ -20,8 +19,27 @@ def upload_to_sqlite():
     con.close()
 
 
+def get_geolocation():
+    
+    with open('sessions.csv') as f:
+        reader = csv.DictReader(f)
+        for i in reader:    
+            ip = i['ip_address'].rpartition(':')[0]    # get IP
+            
+            URL = f'https://api.ipgeolocationapi.com/geolocate/{ip}'
+            r = requests.get(url = URL) 
+            data = r.json() 
+
+            continent = data['continent']
+            country = data['name']
+            print(continent, country)
+
+            # write to db  
+
+
 def main():
-    upload_to_sqlite()
+    # upload_to_sqlite()
+    get_geolocation()
 
 
 if __name__ == '__main__':
